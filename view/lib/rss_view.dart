f9bfa436d46040202cef54949b22e00cbb12289c
ItoYo16u <ito.yo16uh90616@gmail.com>
2021-08-28 22:35:07 +0900

    fix

--- view/lib/rss_view.dart
+++ view/lib/rss_view.dart
@@ -3,4 +3,5 @@ library rss_view;
 export 'functionality/presenter/rss_articles_presenter.dart';
 export 'functionality/presenter/rss_channel_presenter.dart';
 export 'functionality/presenter/rss_list_presenter.dart';
+export 'functionality/presenter/rss_story_presenter.dart';
 export 'ui/channel_thumbnail.dart';
